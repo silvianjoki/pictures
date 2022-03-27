04319aa4ae9c1ecdb8f45bdf871e9d3a6f422fc7
silvianjoki <10silvianjoki@gmail.com>
2022-03-27 17:07:04 +0300

    add migrations

--- apictures/urls.py
+++ apictures/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+from django.urls import path, re_path
+
 from . import views
 
 urlpatterns=[
-    url('^$',views.home,name = 'home'),
+    re_path('^$',views.home,name = 'home'),
     
 ]
--- gallery/urls.py
+++ gallery/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^apictures/',include('apictures.urls'))
+    re_path('^$',include('apictures.urls'))
 ]
