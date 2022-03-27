7a6ab545020eb9444162704ec1f5541fc9e837b6
silvianjoki <10silvianjoki@gmail.com>
2022-03-27 16:36:58 +0300

    edit views

--- apictures/views.py
+++ apictures/views.py
@@ -1,3 +1,84 @@
+import numpy as np
 from django.shortcuts import render
+from django.http  import HttpResponse
+from .models import Image, Category, Location
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+def home(request):
+    images = Image.objects.all()
+    print(images[0].category)
+    arr= np.array(images) 
+    newarr = np.array_split(arr,3)
+    first = newarr[0]
+    second = newarr[1]
+    third = newarr[2]
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'home.html', {"first": first,"second": second,"third": third,"locations": locations,"categories": categories})
+
+def search_results(request):
+    if 'category' in request.GET and request.GET["category"]:
+        search = request.GET.get("category")
+        try:
+            category = Category.objects.get(name = search)
+            images = Image.search_image(category)
+            arr= np.array(images) 
+            newarr = np.array_split(arr,3)
+            first = newarr[0]
+            second = newarr[1]
+            third = newarr[2]
+            message = f"Found {len(images)} image(s) under the category - {search}"
+            return render(request, 'search.html',{"message":message,"images": images,"first": first,"second": second,"third": third})
+        except ObjectDoesNotExist:
+            message = "NO ITEMS UNDER CATEGORY " + search.upper()
+            categories = Category.objects.all()
+            return render(request, 'search.html',{"message":message, "categories": categories}) 
+
+    else:
+        message = "You haven't searched for any category"
+        return render(request, 'search.html',{"message":message})
+
+def image(request, image_id):
+    try:
+        image = Image.objects.get(id=image_id)
+        print(image.category.id)
+    except ObjectDoesNotExist:
+        message = "Image does not exist or may have been deleted!"
+        return render(request, 'image.html', {"message":message})
+    return render(request, 'image.html', {"image":image})
+
+def category(request, category_id):
+    try:
+        category = Category.objects.get(id = category_id)
+        images = Image.search_image(category)
+        arr= np.array(images) 
+        newarr = np.array_split(arr,3)
+        first = newarr[0]
+        second = newarr[1]
+        third = newarr[2]
+        message = category.name
+        title = category.name
+        return render(request, 'search.html',{"title":title, "message":message,"images": images,"first": first,"second": second,"third": third})
+    except ObjectDoesNotExist:
+        message = "NO ITEMS UNDER CATEGORY " + search.upper()
+        categories = Category.objects.all()
+        title= "Not Found"
+        return render(request, 'search.html',{"title":title,"message":message, "categories": categories})
+
+def location(request, location_id):
+    try:
+        location = Location.objects.get(id=location_id)
+        images = Image.filter_by_location(location)
+        arr= np.array(images) 
+        newarr = np.array_split(arr,3)
+        first = newarr[0]
+        second = newarr[1]
+        third = newarr[2]
+        message = location.name
+        title = location.name
+        return render(request, 'search.html',{"title":title, "message":message,"images": images,"first": first,"second": second,"third": third})
+    except ObjectDoesNotExist:
+        message = "NO ITEMS FOR THAT LOCATION"
+        locations = Location.objects.all()
+        title= "Not Found"
+        return render(request, 'search.html',{"title":title,"message":message, "locations": locations})
--- gallery/settings.py
+++ gallery/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,14 +32,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap5'
+    'apictures',
+    'bootstrap5',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apictures.apps.ApicturesConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -77,8 +80,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pict',
+        'USER': 'silvianjoki',
+        'PASSWORD':'password',
     }
 }
 
@@ -107,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -118,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
